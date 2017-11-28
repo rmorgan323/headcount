a2dcbf2a9e164e1061900b217734b20e9e60b977
Rob Morgan <rmorgan323@gmail.com>
2017-11-27 17:26:25 -0700

    passes iteration 0 tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import kinderData from '../data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper';
+
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			data: new DistrictRepository(kinderData)
+		}
+	}
+
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,26 @@
-export default class DistrictRepository {
 
+
+
+export default class DistrictRepository {
+	constructor(rawData) {
+		
+		this.data = []
+		this.cleanData = this.cleanData(rawData)
+		console.log(this.data)
+	}
+
+	cleanData(rawData) {
+		this.data = rawData.reduce((cleanObj, dataObj) => {
+			let loc = dataObj.Location
+
+			if (!cleanObj[loc]) {
+				cleanObj[loc] = {}
+			}
+
+			cleanObj[loc][dataObj.TimeFrame] = dataObj.Data
+
+			return cleanObj
+		}, {})
+
+	}
 }
